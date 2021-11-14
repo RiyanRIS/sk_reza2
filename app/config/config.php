83550b668f7d4d00372343b8c2fcbafad44921f4
riyanris <riyanrisky129@gmail.com>
2021-11-14 20:54:36 +0700

    update database

--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('base_url', 'http://localhost/reza/index.php');
+define('base_url', 'https://mysterious-retreat-39792.herokuapp.com/index.php');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'riyanris');
-define('DB_PASS', '1234');
-define('DB_NAME', 'sk_reza');
+define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
+define('DB_USER', 'b7dd94c1174a22');
+define('DB_PASS', '2091cbc2');
+define('DB_NAME', 'heroku_29c387077802bf6');
