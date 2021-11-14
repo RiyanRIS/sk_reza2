f65ed4b5332667168f76d9e1a0efdc2ecf8025c8
riyanris <riyanrisky129@gmail.com>
2021-11-14 22:01:32 +0700

    add to homeschreen

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- <meta http-equiv='refresh' content='0; URL=/public/index.php'> -->
+        <link rel="icon" type="image/png" href="uploads/icons/favicon.png">
+        <link rel="manifest" href="manifest.json">
+        <link rel="apple-touch-icon" href="uploads/icons/apple_icon_192.png">
+        <meta name="apple-mobile-web-app-status-bar" content="#7367f0">
+        <meta name="theme-color" content="#7367f0">
 
     </head>
     <body>
