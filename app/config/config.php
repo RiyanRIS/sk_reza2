78b586dbf40e3e9178647a4991a8ff5b7435d07e
riyanris <riyanrisky129@gmail.com>
2021-11-22 16:11:22 +0700

    rev 1

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,15 @@
 <?php
 
-define('base_url', 'http://localhost/reza/index.php');
+// define('base_url', 'http://localhost/reza/index.php');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'riyanris');
-define('DB_PASS', '1234');
-define('DB_NAME', 'sk_reza');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'riyanris');
+// define('DB_PASS', '1234');
+// define('DB_NAME', 'sk_reza');
 
-// define('base_url', 'https://arsipbem.herokuapp.com/index.php');
+define('base_url', 'https://arsipbem.herokuapp.com/index.php');
 
-// define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
-// define('DB_USER', 'b7dd94c1174a22');
-// define('DB_PASS', '2091cbc2');
-// define('DB_NAME', 'heroku_29c387077802bf6');
+define('DB_HOST', 'us-cdbr-east-04.cleardb.com');
+define('DB_USER', 'b7dd94c1174a22');
+define('DB_PASS', '2091cbc2');
+define('DB_NAME', 'heroku_29c387077802bf6');
