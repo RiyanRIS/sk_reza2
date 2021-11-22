458758ced892a739d6fd3218ec67d69eb2c6c608
riyanris <riyanrisky129@gmail.com>
2021-11-22 16:09:54 +0700

    rev 1

--- sk_reza.sql
+++ sk_reza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 17, 2021 at 09:59 PM
+-- Generation Time: Nov 22, 2021 at 04:05 PM
 -- Server version: 8.0.26-0ubuntu0.20.04.2
 -- PHP Version: 7.4.3
 
@@ -30,26 +30,65 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `arsip` (
   `id` int NOT NULL,
-  `kode` varchar(64)  NOT NULL,
+  `kode` varchar(64)   NOT NULL,
   `nama` varchar(64) NOT NULL,
   `jenis` varchar(32) NOT NULL,
   `tanggal` date NOT NULL,
   `jam` time NOT NULL,
-  `file` varchar(255) NOT NULL,
-  `catatan` varchar(500)  DEFAULT NULL,
+  `file` varchar(255)   DEFAULT NULL,
+  `catatan` varchar(500)   DEFAULT NULL,
   `id_users` int DEFAULT NULL,
   `created_at` int NOT NULL
-)   ;
+)  ;
 
 --
 -- Dumping data for table `arsip`
 --
 
 INSERT INTO `arsip` (`id`, `kode`, `nama`, `jenis`, `tanggal`, `jam`, `file`, `catatan`, `id_users`, `created_at`) VALUES
-(2, '001/SURATKELUAR/BEM/2021', 'SURAT KELUAR DPM', 'Surat', '2021-02-28', '16:15:00', '1637158664-001 SKELUAR_DPM.jpeg', 'Surat keluar dari BEM untuk DPM dalam kegiatan pelantikan Anggota BEM Periode 2020/2021', NULL, 1628835472),
-(3, '002/SURATMASUK/BEM/2021', 'SURAT MASUK HMJ TK', 'Surat', '2021-12-24', '15:00:00', '1637158583-002 SMASUK_BEM.jpeg', 'Surat masuk ini diberikan oleh HMJ TK dalam hal kegiatan Basic Training Leadership HMJ TK', NULL, 1630826722),
-(4, '003/LPJ/BEM/2021', 'LPL PELANTIKAN BEM', 'Laporan', '2021-03-02', '14:12:00', '1637158911-003 LPJ_PELANTIKAN.pdf', 'Laporan Pertanggungjawaban Pelantikan BEM Periode 2020/2021', NULL, 1636031894),
-(5, '004/PROPOSAL/BEM/2021', 'PROPOSAL FBB 2021', 'Laporan', '2021-07-12', '13:21:00', '1637159286-004 PROPOSAL_FBB.pdf', 'Proposal Forum Belajar BEM (FBB) ini merupakan proposal kegiatan yang dilaksanakan oleh BEM', NULL, 1636190606);
+(2, '001/SURATKELUAR/BEM/2021', 'SURAT KELUAR', 'Surat', '2021-02-28', '16:15:00', '1637158664-001 SKELUAR_DPM.jpeg', 'Surat keluar dari BEM untuk DPM dalam kegiatan pelantikan Anggota BEM Periode 2020/2021', 7, 1628835472),
+(3, '002/SURATMASUK/BEM/2021', 'SURAT MASUK', 'Surat', '2021-12-24', '15:00:00', '1637158583-002 SMASUK_BEM.jpeg', 'Surat masuk ini diberikan oleh HMJ TK dalam hal kegiatan Basic Training Leadership HMJ TK', 7, 1630826722),
+(4, '003/LPJ/BEM/2021', 'LAPORAN PERTANGGUNGJAWABAN', 'Laporan', '2021-03-02', '14:12:00', '1637158911-003 LPJ_PELANTIKAN.pdf', 'Laporan Pertanggungjawaban Pelantikan BEM Periode 2020/2021', 7, 1636031894),
+(5, '004/PROPOSAL/BEM/2021', 'PROPOSAL', 'Laporan', '2021-07-12', '13:21:00', '1637159286-004 PROPOSAL_FBB.pdf', 'Proposal Forum Belajar BEM (FBB) ini merupakan proposal kegiatan yang dilaksanakan oleh BEM', 7, 1636190606);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_arsip`
+--
+
+CREATE TABLE `detail_arsip` (
+  `id` int NOT NULL,
+  `code` int DEFAULT NULL,
+  `nama_file` varchar(322) NOT NULL
+)  ;
+
+--
+-- Dumping data for table `detail_arsip`
+--
+
+INSERT INTO `detail_arsip` (`id`, `code`, `nama_file`) VALUES
+(1, NULL, '1637564353-Screenshot from 2021-02-09 20-28-06.png'),
+(5, 2, '001 SKELUAR_U_DPM.jpg'),
+(6, 2, '002 SKELUAR_PD_LISA DWI RAHMAYANTI.jpg'),
+(7, 3, '001 SMASUK_U_FORKOM BTI.pdf'),
+(8, 2, '003 SKELUAR_PD_MUHAMMAD IKSANNUDIN.jpg'),
+(9, 2, '004 SKELUAR_PD_NOFI YANTI.jpg'),
+(10, 2, '005 SKELUAR_U_HMJ TI.jpg'),
+(11, 3, '002 SMASUK_U_HMJ TK.jpg'),
+(12, 3, '003 SMASUK_P MAAF_HMJ SI.jpg'),
+(13, 3, '004 SMASUK_SE_STMIK AKAKOM.pdf'),
+(14, 3, '005 SMASUK_U_DPM.pdf'),
+(15, 4, '001 LPJ_PELANTIKAN.pdf'),
+(16, 4, '002 LPJ_FBB_BEM.pdf'),
+(17, 4, '003 LPJ_MAKRAB_BEM.pdf'),
+(18, 4, '004 LPJ_SDM_BEM.pdf'),
+(19, 4, '005 LPJ_BNNP DIY_BEM.pdf'),
+(20, 5, '001 PROPOSAL_MAKRAB_BEM.pdf'),
+(21, 5, '002 PROPOSAL_SDM_BEM.pdf'),
+(22, 5, '003 PROPOSAL_SDM MAKRAB_BEM.pdf'),
+(23, 5, '004 PROPOSAL_FBB_BEM.pdf'),
+(24, 5, '005 PROPOSAL_BEM.pdf');
 
 -- --------------------------------------------------------
 
@@ -59,10 +98,10 @@ INSERT INTO `arsip` (`id`, `kode`, `nama`, `jenis`, `tanggal`, `jam`, `file`, `c
 
 CREATE TABLE `periode` (
   `id` int NOT NULL,
-  `tahun` varchar(16)  NOT NULL,
+  `tahun` varchar(16)   NOT NULL,
   `nama` varchar(255) NOT NULL,
   `sk` varchar(255) NOT NULL
-)   ;
+)  ;
 
 --
 -- Dumping data for table `periode`
@@ -87,7 +126,7 @@ CREATE TABLE `profil` (
   `logo_lembaga` varchar(255) NOT NULL,
   `logo_kampus` varchar(255) NOT NULL,
   `updated_at` int NOT NULL
-)   ;
+)  ;
 
 --
 -- Dumping data for table `profil`
@@ -108,7 +147,7 @@ CREATE TABLE `users` (
   `role` varchar(16) NOT NULL,
   `username` varchar(32) NOT NULL,
   `password` varchar(255) NOT NULL
-)   ;
+)  ;
 
 --
 -- Dumping data for table `users`
@@ -128,6 +167,13 @@ INSERT INTO `users` (`id`, `nama`, `role`, `username`, `password`) VALUES
 ALTER TABLE `arsip`
   ADD PRIMARY KEY (`id`),
   ADD KEY `arsip_users` (`id_users`);
+
+--
+-- Indexes for table `detail_arsip`
+--
+ALTER TABLE `detail_arsip`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `arsip_detail` (`code`);
 
 --
 -- Indexes for table `periode`
@@ -155,7 +201,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `arsip`
 --
 ALTER TABLE `arsip`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `detail_arsip`
+--
+ALTER TABLE `detail_arsip`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `periode`
@@ -184,6 +236,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `arsip`
   ADD CONSTRAINT `arsip_users` FOREIGN KEY (`id_users`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `detail_arsip`
+--
+ALTER TABLE `detail_arsip`
+  ADD CONSTRAINT `arsip_detail` FOREIGN KEY (`code`) REFERENCES `arsip` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
